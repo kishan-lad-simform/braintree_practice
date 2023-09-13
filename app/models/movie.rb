1e3c1dfbee3bf4a3c399a3220a5100ff33e1159a
Kishan lad <kishan.l@simformsolution.com>
2023-09-13 17:46:58 +0530

    UNT-T18834:braintree_integration:Feat:adding purchase model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   mount_uploader :movie_image, MovieImageUploader
+  has_many :purchases
+  has_many :buyers, through: :purchases
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_103811) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_113646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_103811) do
     t.string "movie_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "buyer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id", "buyer_id"], name: "index_purchases_on_movie_id_and_buyer_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
