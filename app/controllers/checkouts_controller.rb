d8a56fd60c877b8528fd92c18185fe9f63bfa91b
Kishan lad <kishan.l@simformsolution.com>
2023-09-29 11:49:43 +0530

    UNT-T18834:braintree-integration:Fix:Refector code.

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,29 +1,14 @@
 class CheckoutsController < ApplicationController
-  # before_action :find_movie, only: [ :new, :create ]
-  TRANSACTION_SUCCESS_STATUSES = [
-    Braintree::Transaction::Status::Authorizing,
-    Braintree::Transaction::Status::Authorized,
-    Braintree::Transaction::Status::Settled,
-    Braintree::Transaction::Status::SettlementConfirmed,
-    Braintree::Transaction::Status::SettlementPending,
-    Braintree::Transaction::Status::Settling,
-    Braintree::Transaction::Status::SubmittedForSettlement,
-  ]
-
+  
   def new
     @client_token = Braintree::ClientToken.generate
     find_movie(params[:movie_id])
   end
 
-  def show
-    @transaction = Braintree::Transaction.find(params[:id])
-    @result = _create_result_hash(@transaction)
-  end
-
   def create
     @movie = params[:movie_id]
     find_movie(@movie)
-    amount = @price# In production you should not take amounts directly from clients
+    amount = @price
     nonce = params["payment_method_nonce"]
     result = Braintree::Transaction.sale(
       amount: amount,
@@ -32,46 +17,48 @@ class CheckoutsController < ApplicationController
         :submit_for_settlement => true
       }
     )
-    
-    if result.success? || result.transaction
+    if transaction_successful?(result.transaction)
       redirect_to checkout_path(result.transaction.id)
     else
-      error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
+      handle_transaction_errors(result.errors)
+      render :new
+    end
+  end
+
+  def show
+    @transaction = Braintree::Transaction.find(params[:id])
+    @result = _create_result_hash(@transaction)
+  end
+
+  private
+    def find_movie(id)
+      @movie = Movie.find(id)
+      @price = @movie.price
+    end
+
+    def transaction_successful?(transaction)
+       TRANSACTION_SUCCESS_STATUSES.include?(transaction.status)
+    end
+
+    def _create_result_hash(transaction)
+      status = transaction.status
+      if transaction_successful?(transaction)
+        result_hash = {
+          header: "Sweet Success!",
+          icon: "success",
+          message: "Your test transaction has been successfully processed."
+        }
+      else
+        result_hash = {
+          header: "Transaction Failed",
+          icon: "fail",
+          message: "Your test transaction has a status of #{status}."
+        }
+      end
+    end
+
+    def handle_transaction_errors(errors)
+      error_messages = errors.map { |error| "Error: #{error.code}: #{error.message}" }
       flash[:error] = error_messages
-      render new_checkout_url(@movie_id)
     end
-  end
-
-  def _create_result_hash(transaction)
-    status = transaction.status
-
-    if TRANSACTION_SUCCESS_STATUSES.include? status
-      result_hash = {
-        header: "Sweet Success!",
-        icon: "success",
-        message: "Your test transaction has been successfully processed. See the Braintree API response and try again."
-      }
-    else
-      result_hash = {
-        header: "Transaction Failed",
-        icon: "fail",
-        message: "Your test transaction has a status of #{status}. See the Braintree API response and try again."
-      }
-    end
-  end
-
-  def gateway
-    env = ENV["BT_ENVIRONMENT"]
-    @gateway ||= Braintree::Gateway.new(
-      :environment => env && env.to_sym,
-      :merchant_id => ENV["MERCHANT_ID"],
-      :public_key => ENV["PUBLIC_ID"],
-      :private_key => ENV["PRIVATE_ID"],
-    )
-  end
-
-  def find_movie(id)
-    @movie = Movie.find(id)
-    @price = @movie.price
-  end
 end
--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -3,3 +3,13 @@ Braintree::Configuration.logger = Logger.new('log/braintree.log')
 Braintree::Configuration.merchant_id = ENV['MERCHANT_ID']
 Braintree::Configuration.public_key = ENV['PUBLIC_ID']
 Braintree::Configuration.private_key = ENV['PRIVATE_ID']
+
+TRANSACTION_SUCCESS_STATUSES = [
+  Braintree::Transaction::Status::Authorizing,
+  Braintree::Transaction::Status::Authorized,
+  Braintree::Transaction::Status::Settled,
+  Braintree::Transaction::Status::SettlementConfirmed,
+  Braintree::Transaction::Status::SettlementPending,
+  Braintree::Transaction::Status::Settling,
+  Braintree::Transaction::Status::SubmittedForSettlement,
+].freeze
